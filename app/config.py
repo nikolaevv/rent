815182b2f60b852e0de6b8dc401825067f1d36ab
nikolaevv <starlei165@gmail.com>
2021-09-04 02:45:12 +0300

    feat: Create database

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ CHAT_FOLDER_NAME = 'chats'
 CONTRACTS_FOLDER_NAME = 'contracts'
 REPORTS_FOLDER_NAME = 'reports'
 
-URL_BASE = 'http://127.0.0.1:3000/'
+URL_BASE = 'http://127.0.0.1:8080/'
 AIRPORT_WALLET_ADRESS = 'wq-do2dkj330jr389fji3ijj238rw09'
